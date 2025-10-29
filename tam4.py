e4d0782670ea772d1461fdc1a8ac018859618e09
mohadese <mohadesech53@gmail.com>
2025-10-29 20:11:26 +0330

    tam44

--- tam4.py
+++ tam4.py
@@ -1,9 +1,9 @@
 number= input("enter number : ")
 if len(number) ==2 and number.isdigit():
-    num1=int(number[0])
-    num2=int(number[1])
-    result1=num1**num2
-    result2=num2**num1
+    number1=int(number[0])
+    number2=int(number[1])
+    result1=number1**number2
+    result2=number2**number1
     print(f"first digit to the power of second digit : {result1}")
     print(f"secend digit to the power of first digit : {result2}")
 else :
